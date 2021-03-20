53e1b7eb6777feb920c93d2286557a71869dc221
Rafael Cena <rafael.cena@hotmail.com>
2021-03-20 12:27:28 -0300

    deploy

--- config/database.js
+++ config/database.js
@@ -1,9 +1,10 @@
 module.exports = {
-  username: "kckducpx",
-  password: "A3SpQ8VE3eEyCJHX3qZmbzJ4ON0jb_Ln",
-  database: "kckducpx",
-  host: "tuffi.db.elephantsql.com",
-  dialect: "postgres",
+  username: "cenatec",
+  password: "Sofia090209",
+  database: "cenatecnologia",
+  host: "mysql743.umbler.com",
+  port: "41890",
+  dialect: "mysql",
   define: {
     timestamps: true, // set timestamps (created_at and updated_at) for all db tables
     underscored: true, // set snake case isntead of native sequelize pascal/camel case
